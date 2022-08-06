e72d97114abd380671c076f63803af4235d18751
aaronvictor <aaron.victor30@gmail.com>
2022-08-06 19:28:30 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int q = 0;
+        do
+        {
+            q=q+1;
+            System.out.print(q+" ");
+        }
+        while (q<30);
+        System.out.println();
+    }
+}
